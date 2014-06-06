ecf11638e497170aa3c07683360e13ae69b1874a
Brett Minnie <github@bdb-studios.co.uk>
2014-06-06 14:56:57 +0100

    Added in test for larger period with known working days in a month

--- module/DateUtils/test/DateUtilsTest/WorkingDaysTest.php
+++ module/DateUtils/test/DateUtilsTest/WorkingDaysTest.php
@@ -16,6 +16,7 @@ class WorkingDaysTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals($expected->format('d/m/Y'), $result->format('d/m/Y'));
     }
+
     /**
      * know offset, friday, bank holiday monday, we expect tuesday the 6th
      */
@@ -24,6 +25,18 @@ class WorkingDaysTest extends \PHPUnit_Framework_TestCase
         $expected = \DateTime::createFromFormat('d/m/Y', '06/05/2014');
 
         $result = WorkingDays::workingDaysFrom(\DateTime::createFromFormat('d/m/Y','02/05/2014'), 1);
+
+        $this->assertEquals($expected->format('d/m/Y'), $result->format('d/m/Y'));
+    }
+
+    /**
+     * know offset, we expect it to be a working month of 20 days
+     */
+    public function testWorkingDaysForMay2014()
+    {
+        $expected = \DateTime::createFromFormat('d/m/Y', '30/05/2014');
+
+        $result = WorkingDays::workingDaysFrom(\DateTime::createFromFormat('d/m/Y','30/04/2014'), 20);
 
         $this->assertEquals($expected->format('d/m/Y'), $result->format('d/m/Y'));
     }
